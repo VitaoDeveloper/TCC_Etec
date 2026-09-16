8c05c72665db9d29aa12bd3410b2b426f7da3b96
JotinhaGamer22 <pokemagp771@gmail.com>
2026-09-16 14:34:48 -0300

    fix: bump ASSET_VERSION para invalidar cache do theme-extras

--- components/header.php
+++ components/header.php
@@ -54,7 +54,7 @@ if (isset($pdo)) {
 
 require_once dirname(__DIR__) . '/includes/category_icons.php';
 
-$assetVersion = defined('ASSET_VERSION') ? ASSET_VERSION : '20260909b';
+$assetVersion = ASSET_VERSION;
 ?>
 <!DOCTYPE html>
 <html lang="pt-BR">
